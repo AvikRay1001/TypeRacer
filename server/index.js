b419e43ad30407f97b1e55b1dadaf70c496ade6a
AvikRay1001 <avikray1001@gmail.com>
2025-05-20 22:27:07 +0530

    Day3

--- server/index.js
+++ server/index.js
@@ -6,11 +6,24 @@ const mongooose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 
 app.use(express.json());
 
+const DB = "mongodb+srv://avikray1010:Eq1rFqVD2p5n0pc0@cluster0.ho22kii.mongodb.net/";
 
+io.on('connection', (socket) => {
+    console.log(socket.id);
+});
+
+
+
+mongooose.connect(DB).then(() => {
+    console.log("Connection Successful");
+}).catch((err) => {
+    console.log(err)
+});
 
 server.listen(port, "0.0.0.0", () => {
     console.log(`Server started and running on port ${port}`);
